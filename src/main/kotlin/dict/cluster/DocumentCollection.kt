d7dde856aa3e3109fb556d228a55b41d60e8e453
Yaroslav <q.link0.p@gmail.com>
2020-04-16 10:08:44 +0300

    fixed multiworded ranked search in cluster mode

--- src/main/kotlin/dict/cluster/DocumentCollection.kt
+++ src/main/kotlin/dict/cluster/DocumentCollection.kt
@@ -2,6 +2,7 @@ package dict.cluster
 
 import dict.spimi.RankedDocument
 import dict.spimi.RankedDocuments
+import dict.spimi.tokenSequence
 import util.keySet
 
 // DOCS HAVE TO BE SORTED
@@ -9,7 +10,7 @@ data class DocumentCollection(val documents: List<DocumentVec>, val idf: Inverte
     private val averageDocumentLength = documents.sumBy { it.vector.size } / documents.size
 
     fun find(query: String): RankedDocuments {
-        val querySequence = query.splitToSequence("\\W+")
+        val querySequence = query.tokenSequence
         return documents.asSequence()
             .map {
                 val score = bm25score(querySequence, it, idf, averageDocumentLength.toDouble())
